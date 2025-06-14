20640e214deb168065273a4cde4fbf2291a8e070
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-14 21:00:37 +0300

    Create a base player controller class

---
+++ Source/HonkDuckAges/Player/HDEPlayerController.cpp
@@ -0,0 +1,3 @@
+﻿
+
+#include "HDEPlayerController.h"
---
+++ Source/HonkDuckAges/Player/HDEPlayerController.h
@@ -0,0 +1,15 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HDEPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HONKDUCKAGES_API AHDEPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
