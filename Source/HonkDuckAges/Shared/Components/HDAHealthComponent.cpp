006b1cf4f17185950d291e7f1b5a1764ca09e779
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-14 23:00:57 +0300

    Create a health component

---
+++ Source/HonkDuckAges/Shared/Components/HDAHealthComponent.cpp
@@ -0,0 +1,64 @@
+﻿// 
+
+
+#include "HDAHealthComponent.h"
+
+
+UHDAHealthComponent::UHDAHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UHDAHealthComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	CurrentHealth.Value = DefaultHealth;
+	CurrentHealth.MaxValue = DefaultHealth;
+}
+
+bool UHDAHealthComponent::IncreaseHealth(const int32 Amount)
+{
+	if (Amount <= 0 || CurrentHealth.ReachedMaxValue())
+	{
+		return false;
+	}
+
+	CurrentHealth.IncreaseValue(Amount);
+	OnHealthIncreased.Broadcast(this, CurrentHealth, CurrentHealth.GetNormalizedValue());
+	return true;
+}
+
+bool UHDAHealthComponent::DecreaseHealth(const int32 Amount)
+{
+	if (Amount <= 0 || CurrentHealth.ReachedMinValue())
+	{
+		return false;
+	}
+
+	CurrentHealth.DecreaseValue(Amount);
+	CurrentHealth.ClampToMin();
+	OnHealthDecreased.Broadcast(this, CurrentHealth, CurrentHealth.GetNormalizedValue());
+
+	if (CurrentHealth.ReachedMinValue())
+	{
+		OnHealthReachedZero.Broadcast(this);
+	}
+
+	return true;
+}
+
+bool UHDAHealthComponent::IncreaseMaxHealth(const int32 Amount)
+{
+	if (Amount <= 0)
+	{
+		return false;
+	}
+
+	CurrentHealth.IncreaseMaxValue(Amount);
+	OnMaxHealthIncreased.Broadcast(this, CurrentHealth, CurrentHealth.GetNormalizedValue());
+
+	const int32 DeltaHealth = CurrentHealth.MaxValue - CurrentHealth.Value;
+	IncreaseHealth(DeltaHealth);
+	return true;
+}
---
+++ Source/HonkDuckAges/Shared/Components/HDAHealthComponent.h
@@ -0,0 +1,62 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyPropertiesLibrary.h"
+#include "Components/ActorComponent.h"
+#include "HDAHealthComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnHealthChangedDynamicSignature,
+                                               UHDAHealthComponent*, Component,
+                                               const FTrickyPropertyInt&, CurrentHealth,
+                                               float, NormalizedHealth);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthReachedZeroDynamicSignature,
+                                            UHDAHealthComponent*, Component);
+
+UCLASS(ClassGroup=(Gameplay), meta=(BlueprintSpawnableComponent))
+class HONKDUCKAGES_API UHDAHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHDAHealthComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="Health")
+	FOnHealthChangedDynamicSignature OnHealthIncreased;
+
+	UPROPERTY(BlueprintAssignable, Category="Health")
+	FOnHealthChangedDynamicSignature OnHealthDecreased;
+
+	UPROPERTY(BlueprintAssignable, Category="Health")
+	FOnHealthChangedDynamicSignature OnMaxHealthIncreased;
+
+	UPROPERTY(BlueprintAssignable, Category="Health")
+	FOnHealthReachedZeroDynamicSignature OnHealthReachedZero;
+
+	UPROPERTY(EditDefaultsOnly,
+		Category="Health",
+		meta=(ClampMin=1, UIMin=1))
+	int32 DefaultHealth = 100;
+
+	UFUNCTION(BlueprintGetter, Category="Health")
+	FTrickyPropertyInt GetHealth() const { return CurrentHealth; };
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	bool IncreaseHealth(const int32 Amount = 1);
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	bool DecreaseHealth(const int32 Amount = 1);
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	bool IncreaseMaxHealth(const int32 Amount = 1);
+
+private:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetHealth, Category="Health")
+	FTrickyPropertyInt CurrentHealth{0, 100, 100};
+};
