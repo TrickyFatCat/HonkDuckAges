428e353719aa9d30d85885775fa183afa1ce0084
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-14 21:00:23 +0300

    Create a base player character class

---
+++ Source/HonkDuckAges/Player/HDEPlayerCharacter.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include "HDEPlayerCharacter.h"
+
+
+AHDEPlayerCharacter::AHDEPlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AHDEPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHDEPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHDEPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/HonkDuckAges/Player/HDEPlayerCharacter.h
@@ -0,0 +1,23 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "HDEPlayerCharacter.generated.h"
+
+UCLASS(Abstract)
+class HONKDUCKAGES_API AHDEPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AHDEPlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
