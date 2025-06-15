61a4095a9206991aa9db4a933147ca411495cf99
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-15 21:36:49 +0300

    Create game instance

---
+++ Source/HonkDuckAges/Core/HDAGameInstance.cpp
@@ -0,0 +1,4 @@
+﻿// 
+
+
+#include "HDAGameInstance.h"
---
+++ Source/HonkDuckAges/Core/HDAGameInstance.h
@@ -0,0 +1,16 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "HDAGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HONKDUCKAGES_API UHDAGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
