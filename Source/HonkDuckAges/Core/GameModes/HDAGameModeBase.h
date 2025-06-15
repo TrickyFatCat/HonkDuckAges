6bc35b7de58f6a09a4e30049fd486f828a14b50a
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-15 21:41:22 +0300

    Create base game mode

---
+++ Source/HonkDuckAges/Core/GameModes/HDAGameModeBase.h
@@ -0,0 +1,17 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyGameModeBase.h"
+#include "HDAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HONKDUCKAGES_API AHDAGameModeBase : public ATrickyGameModeBase
+{
+	GENERATED_BODY()
+
+};
