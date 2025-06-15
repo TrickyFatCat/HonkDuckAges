e1718bbba972e97162e3e91f69fb53880898bda4
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-15 19:33:06 +0300

    Create a base damage manager component

---
+++ Source/HonkDuckAges/Shared/Components/HDADamageManagerBase.cpp
@@ -0,0 +1,66 @@
+﻿// 
+
+
+#include "HDADamageManagerBase.h"
+
+#include "HDAHealthComponent.h"
+
+
+UHDADamageManagerBase::UHDADamageManagerBase()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UHDADamageManagerBase::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	UWorld* World = GetWorld();
+
+	if (!IsValid(World) || !World->IsGameWorld())
+	{
+		return;
+	}
+
+	HealthComponent = GetOwner()->GetComponentByClass<UHDAHealthComponent>();
+
+	if (!IsValid(HealthComponent))
+	{
+		
+	}
+	
+	GetOwner()->OnTakeAnyDamage.AddUniqueDynamic(this, &UHDADamageManagerBase::HandleDamageTaken);
+}
+
+void UHDADamageManagerBase::SetIsInvulnerable(const bool Value)
+{
+	if (bIsInvulnerable == Value)
+	{
+		return;
+	}
+
+	bIsInvulnerable = Value;
+
+	if (bIsInvulnerable)
+	{
+		OnInvulnerabilityEnabled.Broadcast(this);
+	}
+	else
+	{
+		OnInvulnerabilityDisabled.Broadcast(this);
+	}
+}
+
+void UHDADamageManagerBase::HandleDamageTaken(AActor* DamagedActor,
+                                              float Damage,
+                                              const UDamageType* DamageType,
+                                              AController* InstigatedBy,
+                                              AActor* DamageCauser)
+{
+	if (!IsValid(HealthComponent))
+	{
+		return;
+	}
+
+	HealthComponent->DecreaseHealth(Damage);
+}
---
+++ Source/HonkDuckAges/Shared/Components/HDADamageManagerBase.h
@@ -0,0 +1,53 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HDADamageManagerBase.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInvulnerabilityChangedDynamicSignature,
+                                            UHDADamageManagerBase*, Component);
+
+UCLASS(ClassGroup=(Gameplay), meta=(BlueprintSpawnableComponent))
+class HONKDUCKAGES_API UHDADamageManagerBase : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHDADamageManagerBase();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="DamageManager")
+	FOnInvulnerabilityChangedDynamicSignature OnInvulnerabilityEnabled;
+	
+	UPROPERTY(BlueprintAssignable, Category="DamageManager")
+	FOnInvulnerabilityChangedDynamicSignature OnInvulnerabilityDisabled;
+	
+	UFUNCTION(BlueprintGetter, Category="DamageManager")
+	bool GetIsInvulnerable() const { return bIsInvulnerable; };
+
+	UFUNCTION(BlueprintSetter, Category="DamageManager")
+	void SetIsInvulnerable(const bool Value);
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UHDAHealthComponent> HealthComponent = nullptr;
+
+private:
+	UPROPERTY(VisibleInstanceOnly,
+		BlueprintGetter=GetIsInvulnerable,
+		BlueprintSetter=SetIsInvulnerable,
+		Category="DamageManager")
+	bool bIsInvulnerable = false;
+
+	UFUNCTION();
+	virtual void HandleDamageTaken(AActor* DamagedActor,
+	                               float Damage,
+	                               const UDamageType* DamageType,
+	                               AController* InstigatedBy,
+	                               AActor* DamageCauser);
+};
