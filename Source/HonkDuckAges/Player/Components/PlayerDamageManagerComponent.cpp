f457c6446d12263b14f27cb0878e8c290c15921a
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-15 19:52:19 +0300

    Create a player-specific damage manager component

---
+++ Source/HonkDuckAges/Player/Components/PlayerDamageManagerComponent.cpp
@@ -0,0 +1,62 @@
+﻿// 
+
+
+#include "PlayerDamageManagerComponent.h"
+
+#include "HonkDuckAges/Shared/Components/HDAArmorComponent.h"
+#include "HonkDuckAges/Shared/Components/HDAHealthComponent.h"
+
+
+UPlayerDamageManagerComponent::UPlayerDamageManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UPlayerDamageManagerComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	UWorld* World = GetWorld();
+
+	if (!IsValid(World) || !World->IsGameWorld())
+	{
+		return;
+	}
+
+	ArmorComponent = GetOwner()->GetComponentByClass<UHDAArmorComponent>();
+
+	if (!IsValid(ArmorComponent))
+	{
+	}
+}
+
+void UPlayerDamageManagerComponent::HandleDamageTaken(AActor* DamagedActor,
+                                                      float Damage,
+                                                      const UDamageType* DamageType,
+                                                      AController* InstigatedBy,
+                                                      AActor* DamageCauser)
+{
+	if (!IsValid(ArmorComponent) || !IsValid(HealthComponent))
+	{
+		return;
+	}
+
+	int32 RemainingDamage = Damage;
+	const FTrickyPropertyInt Armor = ArmorComponent->GetArmor();
+	
+	if (!Armor.ReachedMinValue())
+	{
+		if (RemainingDamage > Armor.Value)
+		{
+			RemainingDamage -= Armor.Value;
+			ArmorComponent->DecreaseArmor(Armor.Value);
+		}
+		else
+		{
+			ArmorComponent->DecreaseArmor(RemainingDamage);
+			return;
+		}
+	}
+
+	HealthComponent->DecreaseHealth(RemainingDamage);
+}
---
+++ Source/HonkDuckAges/Player/Components/PlayerDamageManagerComponent.h
@@ -0,0 +1,32 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HonkDuckAges/Shared/Components/HDADamageManagerBase.h"
+#include "PlayerDamageManagerComponent.generated.h"
+
+
+class UHDAArmorComponent;
+
+UCLASS(ClassGroup=(Player), meta=(BlueprintSpawnableComponent))
+class HONKDUCKAGES_API UPlayerDamageManagerComponent : public UHDADamageManagerBase
+{
+	GENERATED_BODY()
+
+public:
+	UPlayerDamageManagerComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+	UPROPERTY()
+	TObjectPtr<UHDAArmorComponent> ArmorComponent = nullptr;
+
+private:
+	virtual void HandleDamageTaken(AActor* DamagedActor,
+	                               float Damage,
+	                               const UDamageType* DamageType,
+	                               AController* InstigatedBy,
+	                               AActor* DamageCauser) override;
+};
