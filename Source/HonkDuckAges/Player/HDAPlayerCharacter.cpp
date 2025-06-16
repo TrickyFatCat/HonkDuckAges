8a8010754aa3d17bf25a3c83977443e72b4e3ce4
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-16 18:16:36 +0300

    Implement basic movement controls

--- Source/HonkDuckAges/Player/HDAPlayerCharacter.cpp
+++ Source/HonkDuckAges/Player/HDAPlayerCharacter.cpp
@@ -1,9 +1,9 @@
-﻿
-
-#include "HDAPlayerCharacter.h"
+﻿#include "HDAPlayerCharacter.h"
 
 #include "Camera/CameraComponent.h"
 #include "Components/HDAPlayerDamageManagerComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "HonkDuckAges/Shared/Components/HDAArmorComponent.h"
 #include "HonkDuckAges/Shared/Components/HDAHealthComponent.h"
 
@@ -24,7 +24,24 @@ AHDAPlayerCharacter::AHDAPlayerCharacter()
 void AHDAPlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	ensureMsgf(DefaultMappingContext != nullptr, TEXT("DefaultMappingContext wasn't set for %s"),
+	           *GetActorNameOrLabel());
+
+	ensureMsgf(MoveAction != nullptr, TEXT("MoveAction wasn't set for %s"), *GetActorNameOrLabel());
+
+	const APlayerController* PlayerController = Cast<APlayerController>(GetController());
+
+	if (IsValid(PlayerController))
+	{
+		UEnhancedInputLocalPlayerSubsystem* EnhancedInputSubsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+		if (EnhancedInputSubsystem)
+		{
+			EnhancedInputSubsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 void AHDAPlayerCharacter::Tick(float DeltaTime)
@@ -35,5 +52,16 @@ void AHDAPlayerCharacter::Tick(float DeltaTime)
 void AHDAPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AHDAPlayerCharacter::Move);
+	}
 }
 
+void AHDAPlayerCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementDirection = Value.Get<FVector2D>();
+	AddMovementInput(GetActorForwardVector(), MovementDirection.X);
+	AddMovementInput(GetActorRightVector(), MovementDirection.Y);
+}
--- Source/HonkDuckAges/Player/HDAPlayerCharacter.h
+++ Source/HonkDuckAges/Player/HDAPlayerCharacter.h
@@ -5,6 +5,9 @@
 #include "GameFramework/Character.h"
 #include "HDAPlayerCharacter.generated.h"
 
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
 class UCameraComponent;
 class UHDAPlayerDamageManagerComponent;
 class UHDAArmorComponent;
@@ -27,6 +30,12 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Inputs")
+	UInputMappingContext* DefaultMappingContext = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Inputs")
+	UInputAction* MoveAction = nullptr;
+	
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
 	TObjectPtr<UHDAHealthComponent> HealthComponent = nullptr;
 
@@ -38,4 +47,8 @@ protected:
 
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
 	TObjectPtr<UCameraComponent> CameraComponent = nullptr;
+
+private:
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
 };
