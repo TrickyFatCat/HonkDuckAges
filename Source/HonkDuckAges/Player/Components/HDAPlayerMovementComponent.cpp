8e4857bd990522da4d16f782c609fcc4a2358316
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-16 22:01:05 +0300

    Create a player movement component

---
+++ Source/HonkDuckAges/Player/Components/HDAPlayerMovementComponent.cpp
@@ -0,0 +1,51 @@
+﻿// 
+
+
+#include "HDAPlayerMovementComponent.h"
+
+
+// Sets default values for this component's properties
+UHDAPlayerMovementComponent::UHDAPlayerMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UHDAPlayerMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+void UHDAPlayerMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UHDAPlayerMovementComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName PropertyName = PropertyChangedEvent.GetPropertyName();
+	const FName JumpHeightName = GET_MEMBER_NAME_CHECKED(UHDAPlayerMovementComponent, JumpHeight);
+	const FName GravityScaleName = GET_MEMBER_NAME_CHECKED(UHDAPlayerMovementComponent, GravityScale);
+
+	if (PropertyName == JumpHeightName || PropertyName == GravityScaleName)
+	{
+		JumpZVelocity = CalculateJumpZVelocity();
+	}
+	
+	const FName JumpZVelocityName = GET_MEMBER_NAME_CHECKED(UHDAPlayerMovementComponent, JumpZVelocity);
+	
+	if (PropertyName == JumpZVelocityName)
+	{
+		JumpHeight = GetMaxJumpHeight();
+	}
+}
+
+float UHDAPlayerMovementComponent::CalculateJumpZVelocity() const
+{
+	return FMath::Sqrt(-2 * GetGravityZ() * JumpHeight);
+}
+
---
+++ Source/HonkDuckAges/Player/Components/HDAPlayerMovementComponent.h
@@ -0,0 +1,37 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "HDAPlayerMovementComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class HONKDUCKAGES_API UHDAPlayerMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHDAPlayerMovementComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Character Movement: Jumping / Falling",
+		meta=(ClampMin=0, UIMin=0, Delta=1, ForceUnits="Centimeters"))
+	float JumpHeight = 100.f;
+
+private:
+	float CalculateJumpHeight() const;
+};
