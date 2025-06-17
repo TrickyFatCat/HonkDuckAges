9772f665e6f789ae40def843735e7a4378040ce0
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-17 22:27:02 +0300

    Implement basic dash logic

--- Source/HonkDuckAges/Player/Components/HDAPlayerMovementComponent.cpp
+++ Source/HonkDuckAges/Player/Components/HDAPlayerMovementComponent.cpp
@@ -4,7 +4,6 @@
 #include "HDAPlayerMovementComponent.h"
 
 
-// Sets default values for this component's properties
 UHDAPlayerMovementComponent::UHDAPlayerMovementComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
@@ -35,13 +34,129 @@ void UHDAPlayerMovementComponent::PostEditChangeProperty(FPropertyChangedEvent& 
 	{
 		JumpZVelocity = CalculateJumpZVelocity();
 	}
-	
+
 	const FName JumpZVelocityName = GET_MEMBER_NAME_CHECKED(UHDAPlayerMovementComponent, JumpZVelocity);
-	
+
 	if (PropertyName == JumpZVelocityName)
 	{
 		JumpHeight = GetMaxJumpHeight();
 	}
+
+	const FName DashDurationName = GET_MEMBER_NAME_CHECKED(UHDAPlayerMovementComponent, DashDuration);
+	const FName DashDistanceName = GET_MEMBER_NAME_CHECKED(UHDAPlayerMovementComponent, DashDistance);
+
+	if (PropertyName == DashDurationName || PropertyName == DashDistanceName)
+	{
+		DashSpeed = DashDistance / DashDuration;
+	}
+}
+
+bool UHDAPlayerMovementComponent::CanEverJump() const
+{
+	return Super::CanEverJump() && !bIsDashing;
+}
+
+void UHDAPlayerMovementComponent::ProcessLanded(const FHitResult& Hit, float remainingTime, int32 Iterations)
+{
+	Super::ProcessLanded(Hit, remainingTime, Iterations);
+
+	bCanDash = !IsDashOnCooldown();
+}
+
+void UHDAPlayerMovementComponent::StartDashing()
+{
+	if (bIsDashing || IsDashOnCooldown() || !bCanDash)
+	{
+		return;
+	}
+
+	bIsDashing = true;
+	bCanDash = false;
+
+	CalculateDashVelocity(Velocity);
+
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	TimerManager.SetTimer(DashDurationTimer,
+	                      this,
+	                      &UHDAPlayerMovementComponent::FinishDashing,
+	                      DashDuration,
+	                      false);
+	TimerManager.SetTimer(DashCooldownTimer,
+	                      this,
+	                      &UHDAPlayerMovementComponent::HandleDashCooldownFinished,
+	                      DashCooldownDuration,
+	                      false);
+}
+
+void UHDAPlayerMovementComponent::FinishDashing()
+{
+	if (!bIsDashing)
+	{
+		return;
+	}
+
+	bIsDashing = false;
+	bCanDash = !IsDashOnCooldown() && !IsFalling();
+}
+
+void UHDAPlayerMovementComponent::HandleDashCooldownFinished()
+{
+	bCanDash = !IsFalling();
+}
+
+float UHDAPlayerMovementComponent::GetDashCooldownElapsedTime() const
+{
+	if (!GetWorld()->IsGameWorld())
+	{
+		return -1.f;
+	}
+
+	const FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	if (!TimerManager.IsTimerActive(DashCooldownTimer))
+	{
+		return -1.f;
+	}
+
+	return TimerManager.GetTimerElapsed(DashCooldownTimer);
+}
+
+float UHDAPlayerMovementComponent::GetDashCooldownRemainingTime() const
+{
+	if (!GetWorld()->IsGameWorld())
+	{
+		return -1.f;
+	}
+
+	const FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	if (!TimerManager.IsTimerActive(DashCooldownTimer))
+	{
+		return -1.f;
+	}
+
+	return TimerManager.GetTimerRemaining(DashCooldownTimer);
+}
+
+bool UHDAPlayerMovementComponent::IsDashOnCooldown() const
+{
+	if (!GetWorld()->IsGameWorld())
+	{
+		return false;
+	}
+
+	const FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	return TimerManager.IsTimerActive(DashCooldownTimer);
+}
+
+void UHDAPlayerMovementComponent::SetCanDash(const bool Value)
+{
+	if (bCanDash == Value)
+	{
+		return;
+	}
+
+	bCanDash = Value;
 }
 
 float UHDAPlayerMovementComponent::CalculateJumpZVelocity() const
@@ -49,3 +164,15 @@ float UHDAPlayerMovementComponent::CalculateJumpZVelocity() const
 	return FMath::Sqrt(-2 * GetGravityZ() * JumpHeight);
 }
 
+void UHDAPlayerMovementComponent::CalculateDashVelocity(FVector& OutVelocity) const
+{
+	FVector Direction = GetOwner()->GetActorForwardVector();
+
+	if (IsMovingOnGround())
+	{
+		Direction = GetOwner()->GetActorRightVector();
+		Direction = Direction.Cross(CurrentFloor.HitResult.Normal);
+	}
+
+	OutVelocity = Direction * DashSpeed;
+}
--- Source/HonkDuckAges/Player/Components/HDAPlayerMovementComponent.h
+++ Source/HonkDuckAges/Player/Components/HDAPlayerMovementComponent.h
@@ -7,7 +7,7 @@
 #include "HDAPlayerMovementComponent.generated.h"
 
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup=(Custom))
 class HONKDUCKAGES_API UHDAPlayerMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
@@ -25,6 +25,32 @@ public:
 private:
 	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
 
+	virtual bool CanEverJump() const override;
+
+	virtual void ProcessLanded(const FHitResult& Hit, float remainingTime, int32 Iterations) override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Character Movement: Dash")
+	void StartDashing();
+
+	UFUNCTION(BlueprintGetter)
+	bool IsDashing() const { return bIsDashing; }
+
+	UFUNCTION(BlueprintPure)
+	float GetDashCooldownElapsedTime() const;
+
+	UFUNCTION(BlueprintPure)
+	float GetDashCooldownRemainingTime() const;
+
+	UFUNCTION(BlueprintPure)
+	bool IsDashOnCooldown() const;
+
+	UFUNCTION(BlueprintGetter)
+	bool GetCanDash() const { return bCanDash; }
+
+	UFUNCTION(BlueprintSetter)
+	void SetCanDash(const bool Value);
+
 protected:
 	UPROPERTY(EditDefaultsOnly,
 		BlueprintReadOnly,
@@ -32,6 +58,50 @@ protected:
 		meta=(ClampMin=0, UIMin=0, Delta=1, ForceUnits="Centimeters"))
 	float JumpHeight = 100.f;
 
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Character Movement: Dash",
+		meta=(ClampMin=0, UIMin=0, Delta=1, ForceUnits="Centimeters"))
+	float DashDistance = 200.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Character Movement: Dash")
+	float DashSpeed = 200.f;
+
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Character Movement: Dash",
+		meta=(ClampMin=0.1, UIMin=0.1, Delta=0.1, ForceUnits="Seconds"))
+	float DashDuration = 0.35f;
+
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Character Movement: Dash",
+		meta=(ClampMin=0, UIMin=0, Delta=1, ForceUnits="Seconds"))
+	float DashCooldownDuration = 2.0f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Character Movement: Dash")
+	bool bIsDashing = false;
+
+	UPROPERTY(VisibleInstanceOnly,
+		BlueprintGetter=GetCanDash,
+		BlueprintSetter=SetCanDash,
+		Category="Character Movement: Dash")
+	bool bCanDash = true;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Character Movement: Dash")
+	FTimerHandle DashDurationTimer;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Character Movement: Dash")
+	FTimerHandle DashCooldownTimer;
+
 private:
-	float CalculateJumpHeight() const;
+	UFUNCTION()
+	void FinishDashing();
+
+	UFUNCTION()
+	void HandleDashCooldownFinished();
+
+	float CalculateJumpZVelocity() const;
+
+	void CalculateDashVelocity(FVector& OutVelocity) const;
 };
