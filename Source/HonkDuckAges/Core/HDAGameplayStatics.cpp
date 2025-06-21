377f30d65258d4cec7a86189b1eff6ffac476ef0
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-21 15:13:26 +0300

    Create a gameplay statics library

---
+++ Source/HonkDuckAges/Core/HDAGameplayStatics.cpp
@@ -0,0 +1,40 @@
+﻿// 
+
+
+#include "HDAGameplayStatics.h"
+
+#include "HonkDuckAges/Shared/Components/HDALifeStateComponent.h"
+
+bool UHDAGameplayStatics::ApplyHeal(AActor* Target, const int32 HealPower)
+{
+	if (!IsValid(Target) || HealPower <= 0)
+	{
+		return false;
+	}
+
+	UHDALifeStateComponent* LifeStateComponent = Target->FindComponentByClass<UHDALifeStateComponent>();
+
+	if (!IsValid(LifeStateComponent))
+	{
+		return false;
+	}
+
+	return LifeStateComponent->IncreaseHealth(HealPower);
+}
+
+bool UHDAGameplayStatics::RestoreArmor(AActor* Target, const int32 Value)
+{
+	if (!IsValid(Target) || Value <= 0)
+	{
+		return false;
+	}
+
+	UHDALifeStateComponent* LifeStateComponent = Target->FindComponentByClass<UHDALifeStateComponent>();
+
+	if (!IsValid(LifeStateComponent))
+	{
+		return false;
+	}
+
+	return LifeStateComponent->IncreaseArmor(Value);
+}
---
+++ Source/HonkDuckAges/Core/HDAGameplayStatics.h
@@ -0,0 +1,24 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "HDAGameplayStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HONKDUCKAGES_API UHDAGameplayStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Heal")
+	static bool ApplyHeal(AActor* Target, const int32 HealPower);
+
+	UFUNCTION(BlueprintCallable, Category="Heal")
+	static bool RestoreArmor(AActor* Target, const int32 Value);
+
+};
