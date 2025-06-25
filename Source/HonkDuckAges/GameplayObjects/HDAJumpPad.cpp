925579348ca89bb2ba50a1ab3f85e0fbe6483267
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-25 22:57:09 +0300

    Create a base jump pad class

---
+++ Source/HonkDuckAges/GameplayObjects/HDAJumpPad.cpp
@@ -0,0 +1,95 @@
+﻿// 
+
+
+#include "HDAJumpPad.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/TextRenderComponent.h"
+#include "HonkDuckAges/Player/Components/HDAPlayerMovementComponent.h"
+
+
+AHDAJumpPad::AHDAJumpPad()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ActivationTriggerComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("ActivationTrigger"));
+	SetRootComponent(ActivationTriggerComponent);
+	ActivationTriggerComponent->SetCollisionProfileName(TEXT("PlayerTrigger"));
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(GetRootComponent());
+
+#if WITH_EDITOR
+	UpVector = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("UpVector"));
+	UpVector->SetupAttachment(GetRootComponent());
+	UpVector->SetRelativeRotation(FRotator(90, 0, 0));
+	UpVector->SetArrowColor(FColor::Purple);
+	UpVector->SetArrowSize(1.f);
+
+	TextDebug = CreateEditorOnlyDefaultSubobject<UTextRenderComponent>(TEXT("TextDebug"));
+	TextDebug->SetupAttachment(UpVector);
+	TextDebug->SetRelativeLocation(FVector(0, 0, -50));
+	TextDebug->SetHorizontalAlignment(EHTA_Center);
+	TextDebug->SetTextRenderColor(FColor::Purple);
+#endif
+}
+
+void AHDAJumpPad::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+#if WITH_EDITOR
+	UpdateDebugInfo();
+#endif
+
+	UWorld* World = GetWorld();
+
+	if (IsValid(World) && World->IsGameWorld())
+	{
+		ActivationTriggerComponent->OnComponentBeginOverlap.AddUniqueDynamic(this, &AHDAJumpPad::HandleTriggerEntered);
+	}
+}
+
+void AHDAJumpPad::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName PropertyName = PropertyChangedEvent.GetPropertyName();
+	const FName JumpHeightName = GET_MEMBER_NAME_CHECKED(AHDAJumpPad, JumpHeight);
+
+	if (PropertyName == JumpHeightName)
+	{
+		UpdateDebugInfo();
+	}
+}
+
+void AHDAJumpPad::HandleTriggerEntered(UPrimitiveComponent* OverlappedComponent,
+									   AActor* OtherActor,
+									   UPrimitiveComponent* OtherComp,
+									   int32 OtherBodyIndex,
+									   bool bFromSweep,
+									   const FHitResult& SweepResult)
+{
+	if (!IsValid(OtherActor))
+	{
+		return;
+	}
+
+	UHDAPlayerMovementComponent* MovementComponent = OtherActor->FindComponentByClass<UHDAPlayerMovementComponent>();
+
+	if (!IsValid(MovementComponent))
+	{
+		return;
+	}
+
+	MovementComponent->ForceLaunch(JumpHeight, GetActorUpVector());
+}
+
+#if WITH_EDITOR
+void AHDAJumpPad::UpdateDebugInfo() const
+{
+	UpVector->SetArrowLength(JumpHeight);
+	TextDebug->SetText(FText::FromString(FString::Printf(TEXT("Jump Height: %.2f m"), JumpHeight / 100)));
+}
+#endif
---
+++ Source/HonkDuckAges/GameplayObjects/HDAJumpPad.h
@@ -0,0 +1,58 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HDAJumpPad.generated.h"
+
+class UBoxComponent;
+class UArrowComponent;
+class UTextRenderComponent;
+
+UCLASS()
+class HONKDUCKAGES_API AHDAJumpPad : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AHDAJumpPad();
+
+protected:
+	virtual void PostInitializeComponents() override;
+
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UBoxComponent> ActivationTriggerComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UMeshComponent> MeshComponent = nullptr;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadOnly,
+		Category="JumpPad",
+		meta=(ClampMin=100, UIMin=100, Delta=100, ForceUnits="Centimeters"))
+	float JumpHeight = 100.0f;
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(BlueprintReadOnly, Category="Components")
+	TObjectPtr<UArrowComponent> UpVector = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<UTextRenderComponent> TextDebug = nullptr;
+#endif
+
+private:
+	UFUNCTION()
+	void HandleTriggerEntered(UPrimitiveComponent* OverlappedComponent,
+	                          AActor* OtherActor,
+	                          UPrimitiveComponent* OtherComp,
+	                          int32 OtherBodyIndex,
+	                          bool bFromSweep,
+	                          const FHitResult& SweepResult);
+#if WITH_EDITOR
+	void UpdateDebugInfo() const;
+#endif
+};
