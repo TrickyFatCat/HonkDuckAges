dc9837a2d19480c0dddbee58580f50a25066056c
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-26 22:13:11 +0300

    Jump Pads. Add state management and improve debug information

--- Source/HonkDuckAges/GameplayObjects/HDAJumpPad.cpp
+++ Source/HonkDuckAges/GameplayObjects/HDAJumpPad.cpp
@@ -6,6 +6,8 @@
 #include "Components/ArrowComponent.h"
 #include "Components/BoxComponent.h"
 #include "Components/TextRenderComponent.h"
+#include "Door/DoorInterface.h"
+#include "GameplayObject/GameplayObjectStateControllerComponent.h"
 #include "HonkDuckAges/Player/Components/HDAPlayerMovementComponent.h"
 
 
@@ -20,6 +22,9 @@ AHDAJumpPad::AHDAJumpPad()
 	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
 	MeshComponent->SetupAttachment(GetRootComponent());
 
+	StateControllerComponent = CreateDefaultSubobject<UGameplayObjectStateControllerComponent>(
+		TEXT("StateControllerComponent"));
+
 #if WITH_EDITOR
 	UpVector = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("UpVector"));
 	UpVector->SetupAttachment(GetRootComponent());
@@ -29,7 +34,9 @@ AHDAJumpPad::AHDAJumpPad()
 
 	TextDebug = CreateEditorOnlyDefaultSubobject<UTextRenderComponent>(TEXT("TextDebug"));
 	TextDebug->SetupAttachment(UpVector);
-	TextDebug->SetRelativeLocation(FVector(0, 0, -50));
+	TextDebug->SetHiddenInGame(true);
+	TextDebug->SetWorldSize(22);
+	TextDebug->SetRelativeLocation(FVector(20, 0, -70));
 	TextDebug->SetHorizontalAlignment(EHTA_Center);
 	TextDebug->SetTextRenderColor(FColor::Purple);
 #endif
@@ -48,6 +55,7 @@ void AHDAJumpPad::PostInitializeComponents()
 	if (IsValid(World) && World->IsGameWorld())
 	{
 		ActivationTriggerComponent->OnComponentBeginOverlap.AddUniqueDynamic(this, &AHDAJumpPad::HandleTriggerEntered);
+		StateControllerComponent->OnGameplayObjectStateChanged.AddUniqueDynamic(this, &AHDAJumpPad::HandleStateChanged);
 	}
 }
 
@@ -62,14 +70,27 @@ void AHDAJumpPad::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyC
 	{
 		UpdateDebugInfo();
 	}
+
+	const FName InitialStateName = GET_MEMBER_NAME_CHECKED(AHDAJumpPad, InitialState);
+
+	if (PropertyName == InitialStateName)
+	{
+		if (InitialState == EGameplayObjectState::Transition)
+		{
+			InitialState = EGameplayObjectState::Active;
+		}
+
+		StateControllerComponent->SetInitialState(InitialState);
+		HandleStateChanged(nullptr, InitialState, true);
+	}
 }
 
 void AHDAJumpPad::HandleTriggerEntered(UPrimitiveComponent* OverlappedComponent,
-									   AActor* OtherActor,
-									   UPrimitiveComponent* OtherComp,
-									   int32 OtherBodyIndex,
-									   bool bFromSweep,
-									   const FHitResult& SweepResult)
+                                       AActor* OtherActor,
+                                       UPrimitiveComponent* OtherComp,
+                                       int32 OtherBodyIndex,
+                                       bool bFromSweep,
+                                       const FHitResult& SweepResult)
 {
 	if (!IsValid(OtherActor))
 	{
@@ -83,13 +104,34 @@ void AHDAJumpPad::HandleTriggerEntered(UPrimitiveComponent* OverlappedComponent,
 		return;
 	}
 
-	MovementComponent->ForceLaunch(JumpHeight, GetActorUpVector());
+	MovementComponent->ForceLaunch(JumpHeight, GetActorUpVector(), true);
+}
+
+void AHDAJumpPad::HandleStateChanged(UGameplayObjectStateControllerComponent* Component,
+                                     const EGameplayObjectState NewState,
+                                     const bool bChangedImmediately)
+{
+	switch (NewState)
+	{
+	case EGameplayObjectState::Active:
+		ActivationTriggerComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		break;
+
+	case EGameplayObjectState::Disabled:
+		ActivationTriggerComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		break;
+	}
 }
 
 #if WITH_EDITOR
 void AHDAJumpPad::UpdateDebugInfo() const
 {
 	UpVector->SetArrowLength(JumpHeight);
-	TextDebug->SetText(FText::FromString(FString::Printf(TEXT("Jump Height: %.2f m"), JumpHeight / 100)));
+	FString DebugText = FString::Printf(TEXT("%s\n"), *GetActorNameOrLabel());
+	DebugText = DebugText.Appendf(TEXT("Jump Height: %.2f m\n"), JumpHeight / 100);
+	const FString StateName = StaticEnum<EGameplayObjectState>()->
+		GetNameStringByValue(static_cast<int64>(InitialState));
+	DebugText = DebugText.Appendf(TEXT("Initial State: %s"), *StateName);
+	TextDebug->SetText(FText::FromString(DebugText));
 }
 #endif
--- Source/HonkDuckAges/GameplayObjects/HDAJumpPad.h
+++ Source/HonkDuckAges/GameplayObjects/HDAJumpPad.h
@@ -4,13 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayObject/GameplayObjectInterface.h"
 #include "HDAJumpPad.generated.h"
 
+class UGameplayObjectStateControllerComponent;
 class UBoxComponent;
 class UArrowComponent;
 class UTextRenderComponent;
 
-UCLASS()
+UCLASS(PrioritizeCategories="JumpPad")
 class HONKDUCKAGES_API AHDAJumpPad : public AActor
 {
 	GENERATED_BODY()
@@ -30,6 +32,15 @@ protected:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
 	TObjectPtr<UMeshComponent> MeshComponent = nullptr;
 
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UGameplayObjectStateControllerComponent> StateControllerComponent = nullptr;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadOnly,
+		Category="JumpPad",
+		meta=(InvalidEnumValues="EGameplayObjectState::Transition"))
+	EGameplayObjectState InitialState = EGameplayObjectState::Active;
+
 	UPROPERTY(EditAnywhere,
 		BlueprintReadOnly,
 		Category="JumpPad",
@@ -37,7 +48,7 @@ protected:
 	float JumpHeight = 100.0f;
 
 #if WITH_EDITORONLY_DATA
-	UPROPERTY(BlueprintReadOnly, Category="Components")
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
 	TObjectPtr<UArrowComponent> UpVector = nullptr;
 
 	UPROPERTY(VisibleDefaultsOnly, Category="Components")
@@ -52,6 +63,11 @@ private:
 	                          int32 OtherBodyIndex,
 	                          bool bFromSweep,
 	                          const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void HandleStateChanged(UGameplayObjectStateControllerComponent* Component,
+	                        const EGameplayObjectState NewState,
+	                        const bool bChangedImmediately);
 #if WITH_EDITOR
 	void UpdateDebugInfo() const;
 #endif
