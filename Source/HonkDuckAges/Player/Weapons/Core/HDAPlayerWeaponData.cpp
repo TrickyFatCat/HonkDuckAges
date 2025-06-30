0ba9fc2d33969f08d0d6376800a6dcc8ae5b5b4f
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-06-30 21:29:14 +0300

    Create a player weapon data asset class

---
+++ Source/HonkDuckAges/Player/Weapons/Core/HDAPlayerWeaponData.cpp
@@ -0,0 +1,4 @@
+﻿// 
+
+
+#include "HDAPlayerWeaponData.h"
---
+++ Source/HonkDuckAges/Player/Weapons/Core/HDAPlayerWeaponData.h
@@ -0,0 +1,74 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "HDAPlayerWeaponData.generated.h"
+
+class AHDAPlayerWeaponBase;
+
+UENUM(BlueprintType)
+enum class EWeaponSlot : uint8
+{
+	Shotgun,
+	SlugShot,
+	HeavyChainGun,
+	MiniRockets,
+	PlasmaBeam,
+	LaserCrossbow,
+	ShieldThrower,
+};
+
+UENUM(BlueprintType)
+enum class EWeaponAmmoType : uint8
+{
+	Gauge,
+	Bullet,
+	Energy,
+	Shield
+};
+
+USTRUCT(BlueprintType)
+struct FWeaponData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponData")
+	TSubclassOf<AHDAPlayerWeaponBase> WeaponClass = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="WeaponData")
+	EWeaponAmmoType AmmoType = EWeaponAmmoType::Gauge;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="WeaponData", meta=(ClampMin=1, UIMin=1, Delta=1))
+	int32 ShotCost = 1;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class HONKDUCKAGES_API UHDAPlayerWeaponData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="WeaponManager", meta=(ReadOnlyKeys, ForceInlineRow))
+	TMap<EWeaponSlot, FWeaponData> WeaponSlots{
+		{EWeaponSlot::Shotgun, FWeaponData{nullptr, EWeaponAmmoType::Gauge, 2}},
+		{EWeaponSlot::SlugShot, FWeaponData{nullptr, EWeaponAmmoType::Gauge, 1}},
+		{EWeaponSlot::HeavyChainGun, FWeaponData{nullptr, EWeaponAmmoType::Bullet, 1}},
+		{EWeaponSlot::MiniRockets, FWeaponData{nullptr, EWeaponAmmoType::Bullet, 1}},
+		{EWeaponSlot::PlasmaBeam, FWeaponData{nullptr, EWeaponAmmoType::Energy, 1}},
+		{EWeaponSlot::LaserCrossbow, FWeaponData{nullptr, EWeaponAmmoType::Energy, 1}},
+		{EWeaponSlot::ShieldThrower, FWeaponData{nullptr, EWeaponAmmoType::Shield, 1}},
+	};
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="WeaponManager", meta=(ReadOnlyKeys, ForceInlineRow))
+	TMap<EWeaponAmmoType, int32> DefaultWeaponAmmo{
+		{EWeaponAmmoType::Gauge, 40},
+		{EWeaponAmmoType::Bullet, 100},
+		{EWeaponAmmoType::Energy, 200},
+		{EWeaponAmmoType::Shield, 20}
+	};
+};
