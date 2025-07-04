74acfb25f930af0b3bc089874029dc8a3952b3b4
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-07-04 22:31:33 +0300

    Create a base ammo pickup class

---
+++ Source/HonkDuckAges/GameplayObjects/Pickups/HDAPickupAmmoBase.cpp
@@ -0,0 +1,37 @@
+﻿// 
+
+
+#include "HDAPickupAmmoBase.h"
+
+#include "HonkDuckAges/Player/Components/HDAPlayerWeaponManager.h"
+
+
+AHDAPickupAmmoBase::AHDAPickupAmmoBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+bool AHDAPickupAmmoBase::CanBeActivated_Implementation(AActor* Activator)
+{
+	if (!IsValid(Activator))
+	{
+		return false;
+	}
+
+	WeaponManager = Activator->GetComponentByClass<UHDAPlayerWeaponManager>();
+
+	if (!WeaponManager.IsValid())
+	{
+		return false;
+	}
+
+	FTrickyPropertyInt Ammo;
+	WeaponManager->GetAmmo(AmmoType, Ammo);
+	
+	return !Ammo.ReachedMaxValue();
+}
+
+void AHDAPickupAmmoBase::HandleActivationSuccess_Implementation(AActor* Activator)
+{
+	WeaponManager->AddAmmo(AmmoType, AmmoToRestore);
+}
---
+++ Source/HonkDuckAges/GameplayObjects/Pickups/HDAPickupAmmoBase.h
@@ -0,0 +1,33 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HonkDuckAges/Player/Weapons/HDAPlayerWeaponData.h"
+#include "Pickup/PickupAutomaticBase.h"
+#include "HDAPickupAmmoBase.generated.h"
+
+class UHDAPlayerWeaponManager;
+
+UCLASS()
+class HONKDUCKAGES_API AHDAPickupAmmoBase : public APickupAutomaticBase
+{
+	GENERATED_BODY()
+
+public:
+	AHDAPickupAmmoBase();
+
+protected:
+	virtual bool CanBeActivated_Implementation(AActor* Activator) override;
+
+	virtual void HandleActivationSuccess_Implementation(AActor* Activator) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Pickup")
+	EWeaponAmmoType AmmoType = EWeaponAmmoType::Gauge;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Pickup", meta=(ClampMin=1, UIMin=1))
+	int32 AmmoToRestore = 1;
+
+	UPROPERTY()
+	TWeakObjectPtr<UHDAPlayerWeaponManager> WeaponManager = nullptr;
+};
