81faae715dccb62d712b9f44894be8cacdd92df2
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-07-04 22:36:19 +0300

    Create a base weapon pickup class

---
+++ Source/HonkDuckAges/GameplayObjects/Pickups/HDAWeaponPickupBase.cpp
@@ -0,0 +1,35 @@
+﻿// 
+
+
+#include "HDAWeaponPickupBase.h"
+
+#include "HonkDuckAges/Player/Components/HDAPlayerWeaponManager.h"
+
+
+AHDAWeaponPickupBase::AHDAWeaponPickupBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+bool AHDAWeaponPickupBase::CanBeActivated_Implementation(AActor* Activator)
+{
+	if (!IsValid(Activator))
+	{
+		return false;
+	}
+
+	WeaponManager = Activator->GetComponentByClass<UHDAPlayerWeaponManager>();
+
+	if (!WeaponManager.IsValid())
+	{
+		return false;
+	}
+
+	return !WeaponManager->HasWeapon(WeaponToGive);
+}
+
+void AHDAWeaponPickupBase::HandleActivationSuccess_Implementation(AActor* Activator)
+{
+	WeaponManager->AddWeapon(WeaponToGive);
+}
+
---
+++ Source/HonkDuckAges/GameplayObjects/Pickups/HDAWeaponPickupBase.h
@@ -0,0 +1,31 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HonkDuckAges/Player/Weapons/HDAPlayerWeaponData.h"
+#include "Pickup/PickupAutomaticBase.h"
+#include "HDAWeaponPickupBase.generated.h"
+
+class UHDAPlayerWeaponManager;
+
+UCLASS()
+class HONKDUCKAGES_API AHDAWeaponPickupBase : public APickupAutomaticBase
+{
+	GENERATED_BODY()
+
+public:
+	AHDAWeaponPickupBase();
+
+protected:
+	virtual bool CanBeActivated_Implementation(AActor* Activator) override;
+
+	virtual void HandleActivationSuccess_Implementation(AActor* Activator) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Pickup")
+	EWeaponSlot WeaponToGive = EWeaponSlot::SlugShot;
+
+private:
+	UPROPERTY()
+	TWeakObjectPtr<UHDAPlayerWeaponManager> WeaponManager = nullptr;
+};
