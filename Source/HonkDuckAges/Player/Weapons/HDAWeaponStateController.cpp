44cfe198a0cfaa51710d8ddd8fae362e736724c3
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-07-05 20:56:32 +0300

    Create `HDAWeaponStateController` component

---
+++ Source/HonkDuckAges/Player/Weapons/HDAWeaponStateController.cpp
@@ -0,0 +1,60 @@
+﻿// 
+
+
+#include "HDAWeaponStateController.h"
+
+
+UHDAWeaponStateController::UHDAWeaponStateController()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UHDAWeaponStateController::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+bool UHDAWeaponStateController::TransitToIdle(const bool bTransitImmediately)
+{
+	return ChangeState(EWeaponState::Idle, bTransitImmediately);
+}
+
+bool UHDAWeaponStateController::TransitToShooting(const bool bTransitImmediately)
+{
+	return ChangeState(EWeaponState::Shooting, bTransitImmediately);
+}
+
+bool UHDAWeaponStateController::TransitToOutOfAmmo(const bool bTransitImmediately)
+{
+	return ChangeState(EWeaponState::OutOfAmmo, bTransitImmediately);
+}
+
+bool UHDAWeaponStateController::TransitToDisabled(const bool bTransitImmediately)
+{
+	return ChangeState(EWeaponState::Disabled, bTransitImmediately);
+}
+
+bool UHDAWeaponStateController::FinishStateTransition()
+{
+	if (CurrentState != EWeaponState::Transition)
+	{
+		return false;
+	}
+
+	return ChangeState(TargetState, true);
+}
+
+bool UHDAWeaponStateController::ChangeState(const EWeaponState NewState, const bool bTransitImmediately)
+{
+	if (CurrentState == NewState || NewState == EWeaponState::Transition)
+	{
+		return false;
+	}
+
+	TargetState = NewState;
+	CurrentState = bTransitImmediately ? TargetState : EWeaponState::Transition;
+	OnWeaponStateChanged.Broadcast(this,
+	                               CurrentState,
+	                               CurrentState == EWeaponState::Transition ? true : bTransitImmediately);
+	return true;
+}
---
+++ Source/HonkDuckAges/Player/Weapons/HDAWeaponStateController.h
@@ -0,0 +1,69 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HDAWeaponStateController.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	Idle,
+	Shooting,
+	OutOfAmmo,
+	Disabled,
+	Transition
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWeaponStateChangedDynamicSignature,
+                                               UHDAWeaponStateController*, Component,
+                                               EWeaponState, NewState,
+                                               bool, bTransitImmediately);
+
+UCLASS(ClassGroup=(PlayerWeapon))
+class HONKDUCKAGES_API UHDAWeaponStateController : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHDAWeaponStateController();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="WeaponStateController")
+	FOnWeaponStateChangedDynamicSignature OnWeaponStateChanged;
+
+	UFUNCTION(BlueprintCallable, Category=("WeaponStateController"))
+	bool TransitToIdle(const bool bTransitImmediately);
+
+	UFUNCTION(BlueprintCallable, Category=("WeaponStateController"))
+	bool TransitToShooting(const bool bTransitImmediately);
+
+	UFUNCTION(BluepirntCallable, Category=("WeaponStateController"))
+	bool TransitToOutOfAmmo(const bool bTransitImmediately);
+	
+	UFUNCTION(BluepirntCallable, Category=("WeaponStateController"))
+	bool TransitToDisabled(const bool bTransitImmediately);
+	
+	UFUNCTION(BlueprintCallable)
+	bool FinishStateTransition();
+
+	UFUNCTION(BlueprintGetter)
+	EWeaponState GetCurrentState() const { return CurrentState; }
+
+	UFUNCTION(BlueprintGetter)
+	EWeaponState GetTargetState() const { return TargetState; }
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetCurrentState, Category="WeaponStateController")
+	EWeaponState CurrentState = EWeaponState::Disabled;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetTargetState, Category="WeaponStateController")
+	EWeaponState TargetState = EWeaponState::Idle;
+
+	UFUNCTION()
+	bool ChangeState(const EWeaponState NewState, const bool bTransitImmediately);
+};
