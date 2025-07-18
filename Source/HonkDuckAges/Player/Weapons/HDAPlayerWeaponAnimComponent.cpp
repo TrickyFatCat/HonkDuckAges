6c13532b70fb4c7a1267aea9e626fe7b8a4cc7f7
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-07-18 21:23:47 +0300

    Create a player anim component to animate weapon recoil and shake

---
+++ Source/HonkDuckAges/Player/Weapons/HDAPlayerWeaponAnimComponent.cpp
@@ -0,0 +1,154 @@
+﻿// 
+
+
+#include "HDAPlayerWeaponAnimComponent.h"
+
+#include "HDAPlayerWeaponBase.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+UHDAPlayerWeaponAnimComponent::UHDAPlayerWeaponAnimComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	bWantsInitializeComponent = true;
+}
+
+
+void UHDAPlayerWeaponAnimComponent::TickComponent(float DeltaTime,
+                                                  ELevelTick TickType,
+                                                  FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	FVector ShakeOffset = FVector::ZeroVector;
+	FRotator ShakeRotation = FRotator::ZeroRotator;
+
+	CalculateShakeOffset(DeltaTime, ShakeOffset, ShakeRotation);
+
+	FVector RecoilOffset = FVector::ZeroVector;
+	FRotator RecoilRotation = FRotator::ZeroRotator;
+
+	CalculateRecoilOffset(DeltaTime, RecoilOffset, RecoilRotation);
+
+	const FRotator RotationOffset = UKismetMathLibrary::ComposeRotators(ShakeRotation, RecoilRotation);
+
+	const FVector NewLocation = InitialLocation + ShakeOffset + RecoilOffset;
+	const FRotator NewRotation = UKismetMathLibrary::ComposeRotators(InitialRotation, RotationOffset);
+	SetRelativeLocationAndRotation(NewLocation, NewRotation);
+}
+
+void UHDAPlayerWeaponAnimComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	UWorld* World = GetWorld();
+
+	if (IsValid(World) && World->IsGameWorld())
+	{
+		InitialLocation = GetRelativeLocation();
+		InitialRotation = GetRelativeRotation();
+		OwningWeapon = Cast<AHDAPlayerWeaponBase>(GetOwner());
+	}
+}
+
+void UHDAPlayerWeaponAnimComponent::AddShake(const float Value)
+{
+	if (Value <= 0.f)
+	{
+		return;
+	}
+
+	ShakeStrength += Value;
+	ShakeStrength = FMath::Clamp(ShakeStrength, 0.f, 1.f);
+}
+
+void UHDAPlayerWeaponAnimComponent::CalculateShakeOffset(const float DeltaTime,
+                                                         FVector& OutLocation,
+                                                         FRotator& OutRotation)
+{
+	if (ShakeStrength <= 0.f)
+	{
+		OutLocation = FVector::ZeroVector;
+		OutRotation = FRotator::ZeroRotator;
+		return;
+	}
+
+	ShakeStrength = FMath::FInterpTo(ShakeStrength, 0.f, DeltaTime, ShakeFadeSpeed);
+	const float ShakeProgress = IsValid(ShakeStrengthCurve)
+		                            ? ShakeStrengthCurve->GetFloatValue(1 - ShakeStrength)
+		                            : ShakeStrength;
+
+	OutLocation = ShakeLocationThreshold * FMath::VRand() * ShakeProgress;
+
+	OutRotation.Roll = ShakeRotationThreshold.Roll * FMath::RandRange(-1.f, 1.f) * ShakeProgress;
+	OutRotation.Pitch = ShakeRotationThreshold.Pitch * FMath::RandRange(-1.f, 1.f) * ShakeProgress;
+	OutRotation.Yaw = ShakeRotationThreshold.Yaw * FMath::RandRange(-1.f, 1.f) * ShakeProgress;
+}
+
+float UHDAPlayerWeaponAnimComponent::GetNormalizedRemainingTime() const
+{
+	if (!OwningWeapon.IsValid())
+	{
+		return -1.f;
+	}
+
+	return OwningWeapon->GetNormalizedRemainingShotTime();
+}
+
+float UHDAPlayerWeaponAnimComponent::GetNormalizedElapsedTime() const
+{
+	if (!OwningWeapon.IsValid())
+	{
+		return -1.f;
+	}
+
+	return OwningWeapon->GetNormalizedElapsedShotTime();
+}
+
+void UHDAPlayerWeaponAnimComponent::CalculateRecoilOffset(const float DeltaTime,
+                                                          FVector& OutLocation,
+                                                          FRotator& OutRotation)
+{
+	if (!OwningWeapon.IsValid())
+	{
+		OutLocation = FVector::ZeroVector;
+		OutRotation = FRotator::ZeroRotator;
+		return;
+	}
+
+	const float ShotTimerProgress = GetNormalizedRemainingTime();
+
+	switch (OwningWeapon->GetWeaponMode())
+	{
+	case EWeaponMode::FullAuto:
+		if (!bRecoilEachShotOnFullAuto)
+		{
+			RecoilTargetStrength = OwningWeapon->GetCurrentState() == EWeaponState::Shooting ? 1.f : 0.f;
+		}
+		else
+		{
+			RecoilTargetStrength = ShotTimerProgress <= RecoilShotDelayThreshold ? 0.f : 1.f;
+		}
+		break;
+	case EWeaponMode::SemiAuto:
+		RecoilTargetStrength = ShotTimerProgress <= RecoilShotDelayThreshold ? 0.f : 1.f;
+		break;
+	}
+
+
+	const float RecoilFadeFactor = IsValid(RecoilFadeCurve)
+		                               ? RecoilFadeCurve->GetFloatValue(1 - ShotTimerProgress)
+		                               : 1.f;
+	
+	RecoilStrength = FMath::FInterpTo(RecoilStrength,
+	                                  RecoilTargetStrength,
+	                                  DeltaTime,
+	                                  RecoilFadeSpeed * RecoilFadeFactor);
+
+	const float RecoilProgress = IsValid(RecoilStrengthCurve)
+		                             ? RecoilStrengthCurve->GetFloatValue(RecoilStrength)
+		                             : RecoilStrength;
+
+	OutLocation = RecoilLocationThreshold * RecoilProgress;
+	OutRotation = RecoilRotationThreshold * RecoilProgress;
+}
---
+++ Source/HonkDuckAges/Player/Weapons/HDAPlayerWeaponAnimComponent.h
@@ -0,0 +1,98 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "HDAPlayerWeaponAnimComponent.generated.h"
+
+
+class AHDAPlayerWeaponBase;
+
+UCLASS(ClassGroup=(Player), PrioritizeCategories=("Shake, Recoil"))
+class HONKDUCKAGES_API UHDAPlayerWeaponAnimComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHDAPlayerWeaponAnimComponent();
+
+	virtual void TickComponent(float DeltaTime,
+	                           ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void AddShake(const float Value);
+
+	UFUNCTION(BlueprintPure)
+	AHDAPlayerWeaponBase* GetOwningWeapon() const { return OwningWeapon.Get(); }
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, Category="Default")
+	TWeakObjectPtr<AHDAPlayerWeaponBase> OwningWeapon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Shake")
+	FVector ShakeLocationThreshold = FVector(2.f);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Shake")
+	FRotator ShakeRotationThreshold = FRotator(2.f);
+
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Shake",
+		meta=(ClampMin=0, UIMin=0, Delta=1))
+	float ShakeFadeSpeed = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Shake")
+	UCurveFloat* ShakeStrengthCurve = nullptr;
+
+	float ShakeStrength = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Recoil")
+	FVector RecoilLocationThreshold = FVector(5.f, 0.f, 0.f);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Recoil")
+	FRotator RecoilRotationThreshold = FRotator(0.f, 15.f, 0.f);
+
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Recoil",
+		meta=(ClampMin=0, UIMin=0, ClampMax=1, UIMax=1, Delta=0.1))
+	float RecoilShotDelayThreshold = 0.5f;
+
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Recoil",
+		meta=(ClampMin=0, UIMin=0, Delta=1))
+	float RecoilFadeSpeed = 12.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Recoil")
+	UCurveFloat* RecoilFadeCurve = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Recoil")
+	UCurveFloat* RecoilStrengthCurve = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Recoil")
+	bool bRecoilEachShotOnFullAuto = true;
+
+	float RecoilStrength = 0.f;
+
+	float RecoilTargetStrength = 0.f;
+
+private:
+	FVector InitialLocation = FVector::ZeroVector;
+
+	FRotator InitialRotation = FRotator::ZeroRotator;
+
+	void CalculateShakeOffset(const float DeltaTime, FVector& OutLocation, FRotator& OutRotation);
+
+	float GetNormalizedRemainingTime() const;
+
+	float GetNormalizedElapsedTime() const;
+
+	void CalculateRecoilOffset(const float DeltaTime, FVector& OutLocation, FRotator& OutRotation);
+};
