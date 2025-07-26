ec83b6614c32310a8e635cec8537ea960dddf33e
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-07-26 18:57:52 +0300

    Create base button class

---
+++ Source/HonkDuckAges/GameplayObjects/Buttons/HDAButtonBase.cpp
@@ -0,0 +1,345 @@
+﻿// 
+
+
+#include "HDAButtonBase.h"
+
+#include "TrickyInteractionLibrary.h"
+#include "TrickyUtilityLibrary.h"
+#include "Button/ButtonStateControllerComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/TextRenderComponent.h"
+#include "Components/TimelineComponent.h"
+
+
+AHDAButtonBase::AHDAButtonBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	InteractionTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionTrigger"));
+	InteractionTrigger->SetupAttachment(GetRootComponent());
+	InteractionTrigger->SetCollisionProfileName(TEXT("PlayerTrigger"));
+	InteractionTrigger->SetMobility(EComponentMobility::Type::Static);
+
+	StateControllerComponent = CreateDefaultSubobject<UButtonStateControllerComponent>(TEXT("StateController"));
+	AnimationComponent = CreateDefaultSubobject<UTimelineComponent>(TEXT("AnimationComponent"));
+	AnimationComponent->SetTimelineLength(1.f);
+
+#if WITH_EDITOR
+	ForwardVector = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("ForwardVector"));
+	ForwardVector->SetupAttachment(GetRootComponent());
+	ForwardVector->SetMobility(EComponentMobility::Type::Static);
+	ForwardVector->SetRelativeLocation(FVector(0.0f, 0.0f, 100.0f));
+	ForwardVector->ArrowLength = 100.0f;
+	ForwardVector->ArrowSize = 2.f;
+
+	InfoText = CreateEditorOnlyDefaultSubobject<UTextRenderComponent>(TEXT("InfoText"));
+	InfoText->SetupAttachment(ForwardVector);
+	InfoText->SetMobility(EComponentMobility::Type::Static);
+	InfoText->SetHiddenInGame(true);
+	InfoText->SetRelativeLocation(FVector(50.0f, 0.0f, 150.f));
+	InfoText->SetHorizontalAlignment(EHTA_Center);
+	InfoText->SetWorldSize(34.f);
+	InfoText->SetTextRenderColor(FColor::Magenta);
+#endif
+}
+
+void AHDAButtonBase::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+#if WITH_EDITOR
+	UpdateInfoText();
+#endif
+
+	UWorld* World = GetWorld();
+
+	if (IsValid(World) || World->IsGameWorld())
+	{
+		ensureMsgf(AnimationCurve != nullptr,
+		           TEXT("AnimationCurve is not set for %s"),
+		           *GetActorNameOrLabel());
+
+		InteractionTrigger->OnComponentBeginOverlap.AddUniqueDynamic(this,
+		                                                             &AHDAButtonBase::HandleTriggerEntered);
+		InteractionTrigger->OnComponentEndOverlap.AddUniqueDynamic(this,
+		                                                           &AHDAButtonBase::HandleTriggerExited);
+
+		StateControllerComponent->OnButtonStateChanged.AddUniqueDynamic(this,
+		                                                                &AHDAButtonBase::HandleStateChanged);
+		StateControllerComponent->OnButtonStateTransitionStarted.AddUniqueDynamic(this,
+			&AHDAButtonBase::HandleTransitionStarted);
+		StateControllerComponent->OnButtonStateTransitionReversed.AddUniqueDynamic(this,
+			&AHDAButtonBase::HandleTransitionReversed);
+
+		if (IsValid(AnimationCurve))
+		{
+			FOnTimelineFloat ButtonAnimationTimelineDelegate;
+			ButtonAnimationTimelineDelegate.BindUFunction(this, FName("ProcessAnimation"));
+			AnimationComponent->AddInterpFloat(AnimationCurve,
+			                                   ButtonAnimationTimelineDelegate,
+			                                   NAME_None,
+			                                   TEXT("Progress"));
+
+			FOnTimelineEvent ButtonAnimationFinishedDelegate;
+			ButtonAnimationFinishedDelegate.BindUFunction(this, FName("FinishAnimation"));
+			AnimationComponent->SetTimelineFinishedFunc(ButtonAnimationFinishedDelegate);
+
+			if (!ensureMsgf(PressAnimationDuration > 0.0,
+			                TEXT("%s animation duration can't be less or equal zero. Force it to 1"),
+			                *GetActorNameOrLabel()))
+			{
+				PressAnimationDuration = 1.0f;
+			}
+
+			UTrickyUtilityLibrary::CalculateTimelinePlayRate(AnimationComponent, PressAnimationDuration);
+		}
+	}
+}
+
+#if WITH_EDITOR
+void AHDAButtonBase::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName PropertyName = PropertyChangedEvent.GetPropertyName();
+	const FName InitialStateName = GET_MEMBER_NAME_CHECKED(AHDAButtonBase, InitialState);
+
+	if (PropertyName == InitialStateName)
+	{
+		StateControllerComponent->SetInitialState(InitialState);
+
+		switch (InitialState)
+		{
+		case EButtonState::Released:
+			InteractionTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+			break;
+
+		case EButtonState::Pressed:
+			InteractionTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+
+		case EButtonState::Disabled:
+			InteractionTrigger->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+			break;
+		}
+	}
+
+	UpdateInfoText();
+}
+#endif
+
+EInteractionResult AHDAButtonBase::ForceInteraction_Implementation(AActor* Interactor)
+{
+	bool bResult = false;
+
+	switch (StateControllerComponent->GetCurrentState())
+	{
+	case EButtonState::Released:
+		bResult = IButtonInterface::Execute_PressButton(StateControllerComponent, false);;
+		break;
+
+	case EButtonState::Pressed:
+		if (ButtonType == EButtonType::Lever)
+		{
+			bResult = IButtonInterface::Execute_ReleaseButton(StateControllerComponent, false);
+		}
+		break;
+	}
+
+	return bResult ? EInteractionResult::Success : EInteractionResult::Failure;
+}
+
+bool AHDAButtonBase::PressButton_Implementation(const bool bTransitImmediately)
+{
+	return IButtonInterface::Execute_PressButton(StateControllerComponent, bTransitImmediately);
+}
+
+bool AHDAButtonBase::ReleaseButton_Implementation(const bool bTransitImmediately)
+{
+	return IButtonInterface::Execute_ReleaseButton(StateControllerComponent, bTransitImmediately);
+}
+
+bool AHDAButtonBase::ReverseButtonStateTransition_Implementation()
+{
+	return IButtonInterface::Execute_ReverseButtonStateTransition(StateControllerComponent);
+}
+
+bool AHDAButtonBase::DisableButton_Implementation(const bool bTransitImmediately)
+{
+	return IButtonInterface::Execute_DisableButton(StateControllerComponent, bTransitImmediately);
+}
+
+bool AHDAButtonBase::EnableButton_Implementation(const bool bTransitImmediately)
+{
+	return IButtonInterface::Execute_EnableButton(StateControllerComponent, bTransitImmediately);
+}
+
+bool AHDAButtonBase::ForceButtonState_Implementation(const EButtonState NewState, const bool bTransitImmediately)
+{
+	return IButtonInterface::Execute_ForceButtonState(StateControllerComponent, NewState, bTransitImmediately);
+}
+
+void AHDAButtonBase::HandleTriggerEntered(UPrimitiveComponent* OverlappedComponent,
+                                          AActor* OtherActor,
+                                          UPrimitiveComponent* OtherComp,
+                                          int32 OtherBodyIndex,
+                                          bool bFromSweep,
+                                          const FHitResult& SweepResult)
+{
+	UTrickyInteractionLibrary::AddToInteractionQueue(OtherActor, this);
+}
+
+void AHDAButtonBase::HandleTriggerExited(UPrimitiveComponent* OverlappedComponent,
+                                         AActor* OtherActor,
+                                         UPrimitiveComponent* OtherComp,
+                                         int32 OtherBodyIndex)
+{
+	UTrickyInteractionLibrary::RemoveFromInteractionQueue(OtherActor, this);
+}
+
+void AHDAButtonBase::HandleStateChanged(UButtonStateControllerComponent* Component,
+                                        const EButtonState NewState,
+                                        const bool bChangedImmediately)
+{
+	switch (NewState)
+	{
+	case EButtonState::Pressed:
+		if (ButtonType == EButtonType::Lever)
+		{
+			InteractionTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		}
+		break;
+	case EButtonState::Released:
+		if (ButtonType == EButtonType::Lever || ButtonType == EButtonType::ButtonKey)
+		{
+			InteractionTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		}
+		break;
+	}
+}
+
+void AHDAButtonBase::HandleTransitionStarted(UButtonStateControllerComponent* Component,
+                                             const EButtonState TargetState)
+{
+	InteractionTrigger->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	CalculateAnimationPlayRate(TargetState);
+
+	switch (TargetState)
+	{
+	case EButtonState::Pressed:
+		AnimationComponent->PlayFromStart();
+		break;
+
+	case EButtonState::Released:
+		AnimationComponent->ReverseFromEnd();
+		break;
+	}
+}
+
+void AHDAButtonBase::HandleTransitionReversed(UButtonStateControllerComponent* Component,
+                                              const EButtonState NewTargetState)
+{
+	CalculateAnimationPlayRate(NewTargetState);
+
+	switch (NewTargetState)
+	{
+	case EButtonState::Pressed:
+		AnimationComponent->Play();
+		break;
+	case EButtonState::Released:
+		AnimationComponent->Reverse();
+		break;
+	}
+}
+
+void AHDAButtonBase::CalculateAnimationPlayRate(const EButtonState State) const
+{
+	if (!bSeparateAnimationDuration)
+	{
+		UTrickyUtilityLibrary::CalculateTimelinePlayRate(AnimationComponent, PressAnimationDuration);
+		return;
+	}
+
+	switch (State)
+	{
+	case EButtonState::Pressed:
+		UTrickyUtilityLibrary::CalculateTimelinePlayRate(AnimationComponent, PressAnimationDuration);
+		break;
+
+	case EButtonState::Released:
+		UTrickyUtilityLibrary::CalculateTimelinePlayRate(AnimationComponent, ReleaseAnimationDuration);
+		break;
+	}
+}
+
+
+void AHDAButtonBase::ProcessAnimation(const float Progress)
+{
+	AnimationProcessed(Progress);
+}
+
+
+void AHDAButtonBase::FinishAnimation()
+{
+	IButtonInterface::Execute_FinishButtonStateTransition(StateControllerComponent);
+	AnimationFinished();
+
+	switch (ButtonType)
+	{
+	case EButtonType::ButtonKey:
+		if (KeyPressDuration > 0.f)
+		{
+			GetWorldTimerManager().SetTimer(KeyPressTimerHandle, this, &AHDAButtonBase::ReleaseKey, KeyPressDuration);
+		}
+		else
+		{
+			ReleaseKey();
+		}
+		break;
+
+	case EButtonType::Normal:
+		DisableButton_Implementation(true);
+		break;
+	}
+}
+
+void AHDAButtonBase::ReleaseKey()
+{
+	if (ButtonType != EButtonType::ButtonKey)
+	{
+		return;
+	}
+
+	Execute_ReleaseButton(this, false);
+}
+
+#if WITH_EDITOR
+void AHDAButtonBase::UpdateInfoText()
+{
+	FString DebugText = FString::Printf(TEXT("%s\n"), *GetActorNameOrLabel());
+	const FString ButtonTypeName = StaticEnum<EButtonType>()->GetNameStringByValue(static_cast<int64>(ButtonType));
+	DebugText = DebugText.Appendf(TEXT("Button Type: %s\n"), *ButtonTypeName);
+	const FString StateName = StaticEnum<EButtonState>()->GetNameStringByValue(static_cast<int64>(InitialState));
+	DebugText = DebugText.Appendf(TEXT("Initial State: %s\n"), *StateName);
+
+	if (!bSeparateAnimationDuration)
+	{
+		DebugText = DebugText.Appendf(TEXT("Animation Duration: %.2f\n"), PressAnimationDuration);
+	}
+	else
+	{
+		DebugText = DebugText.Appendf(TEXT("Press Animation Duration: %.2f\n"), PressAnimationDuration);
+		DebugText = DebugText.Appendf(TEXT("Release Animation Duration: %.2f\n"), ReleaseAnimationDuration);
+	}
+
+	if (ButtonType == EButtonType::ButtonKey)
+	{
+		DebugText = DebugText.Appendf(TEXT("Press Duration: %.2f\n"), KeyPressDuration);
+	}
+
+	InfoText->SetText(FText::FromString(DebugText));
+}
+#endif
---
+++ Source/HonkDuckAges/GameplayObjects/Buttons/HDAButtonBase.h
@@ -0,0 +1,175 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyInteractionInterface.h"
+#include "Button/ButtonInterface.h"
+#include "GameFramework/Actor.h"
+#include "HDAButtonBase.generated.h"
+
+class UTextRenderComponent;
+class UArrowComponent;
+class UTimelineComponent;
+class UButtonStateControllerComponent;
+class UBoxComponent;
+
+UENUM()
+enum class EButtonType : uint8
+{
+	Normal,
+	ButtonKey,
+	Lever
+};
+
+UCLASS(Abstract, PrioritizeCategories="Button")
+class HONKDUCKAGES_API AHDAButtonBase : public AActor, public ITrickyInteractionInterface, public IButtonInterface
+{
+	GENERATED_BODY()
+
+public:
+	AHDAButtonBase();
+
+protected:
+	virtual void PostInitializeComponents() override;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Button")
+	FInteractionData InteractionData{
+		FText::FromString(TEXT("Press")),
+		true,
+		0
+	};
+
+	virtual EInteractionResult ForceInteraction_Implementation(AActor* Interactor) override;
+
+	virtual bool PressButton_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool ReleaseButton_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool ReverseButtonStateTransition_Implementation() override;
+
+	virtual bool DisableButton_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool EnableButton_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool ForceButtonState_Implementation(const EButtonState NewState, const bool bTransitImmediately) override;
+
+	UFUNCTION(BlueprintGetter, Category="Button")
+	float GetKeyPressDuration() const { return KeyPressDuration; }
+
+	UFUNCTION(BlueprintSetter, Category="Button")
+	void SetKeyPressDuration(const float NewDuration) { KeyPressDuration = NewDuration; }
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<USceneComponent> Root = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UBoxComponent> InteractionTrigger = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UButtonStateControllerComponent> StateControllerComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UTimelineComponent> AnimationComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Button")
+	EButtonType ButtonType = EButtonType::Normal;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Button", meta=(InvalidEnumValues="EButtonState::Transition"))
+	EButtonState InitialState = EButtonState::Released;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintGetter=GetKeyPressDuration,
+		BlueprintSetter=SetKeyPressDuration,
+		Category="Button",
+		meta=(ClampMin=0, UIMin=0, Delta=0.5, ForceUnits="Seconds", EditCondition="ButtonType == EButtonType::ButtonKey"
+		))
+	float KeyPressDuration = 0.5f;
+
+	UPROPERTY(BlueprintReadOnly, Category="Button")
+	FTimerHandle KeyPressTimerHandle;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadOnly,
+		Category="Button",
+		meta=(ClampMin=0.0f, UIMin=0.0f, Delta=0.1f, ForceUnits="Seconds"))
+	float PressAnimationDuration = 0.25f;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadOnly,
+		Category="Button")
+	bool bSeparateAnimationDuration = false;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadOnly,
+		Category="Button",
+		meta=(ClampMin=0.0f, UIMin=0.0f, Delta=0.1f, ForceUnits="Seconds",
+			EditCondition="bSeparateAnimationDuration == true"))
+	float ReleaseAnimationDuration = 0.25f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Button")
+	UCurveFloat* AnimationCurve = nullptr;
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Button")
+	void AnimationProcessed(const float Progress);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Button")
+	void AnimationFinished();
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UArrowComponent> ForwardVector = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UTextRenderComponent> InfoText = nullptr;
+#endif
+
+private:
+	UFUNCTION()
+	void HandleTriggerEntered(UPrimitiveComponent* OverlappedComponent,
+	                          AActor* OtherActor,
+	                          UPrimitiveComponent* OtherComp,
+	                          int32 OtherBodyIndex,
+	                          bool bFromSweep,
+	                          const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void HandleTriggerExited(UPrimitiveComponent* OverlappedComponent,
+	                         AActor* OtherActor,
+	                         UPrimitiveComponent* OtherComp,
+	                         int32 OtherBodyIndex);
+
+	UFUNCTION()
+	void HandleStateChanged(UButtonStateControllerComponent* Component,
+	                        const EButtonState NewState,
+	                        const bool bChangedImmediately);
+
+	UFUNCTION()
+	void HandleTransitionStarted(UButtonStateControllerComponent* Component,
+	                             const EButtonState TargetState);
+
+	UFUNCTION()
+	void HandleTransitionReversed(UButtonStateControllerComponent* Component,
+	                              const EButtonState NewTargetState);
+
+	void CalculateAnimationPlayRate(const EButtonState State) const;
+
+	UFUNCTION()
+	void ProcessAnimation(const float Progress);
+
+	UFUNCTION()
+	void FinishAnimation();
+
+	UFUNCTION()
+	void ReleaseKey();
+
+#if WITH_EDITOR
+	void UpdateInfoText();
+#endif
+};
