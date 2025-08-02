13d9a690c323721ba4cd9f20c1006cb4f88c630b
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-08-02 21:58:30 +0300

    Create base enemy class

---
+++ Source/HonkDuckAges/Enemies/HDAEnemyBase.cpp
@@ -0,0 +1,43 @@
+﻿// 
+
+
+#include "HDAEnemyBase.h"
+
+#include "HonkDuckAges/Shared/Components/HDALifeStateComponent.h"
+
+
+AHDAEnemyBase::AHDAEnemyBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	LifeStateComponent = CreateDefaultSubobject<UHDALifeStateComponent>(TEXT("LifeState"));
+}
+
+void AHDAEnemyBase::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	UWorld* World = GetWorld();
+
+	if (IsValid(World) && World->IsGameWorld())
+	{
+		
+	}
+}
+
+void AHDAEnemyBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHDAEnemyBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHDAEnemyBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/HonkDuckAges/Enemies/HDAEnemyBase.h
@@ -0,0 +1,32 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "HDAEnemyBase.generated.h"
+
+class UHDALifeStateComponent;
+
+UCLASS()
+class HONKDUCKAGES_API AHDAEnemyBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AHDAEnemyBase();
+
+protected:
+	virtual void PostInitializeComponents() override;
+	
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UHDALifeStateComponent> LifeStateComponent = nullptr;
+};
